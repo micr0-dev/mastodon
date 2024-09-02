58b82523a76c050d76143162f873b63f37e884dd
micr0byte <26364458+micr0-dev@users.noreply.github.com>
2024-09-02 13:00:30 -0400

    uppercase and other field names

--- app/javascript/flavours/glitch/features/status/components/detailed_status.jsx
+++ app/javascript/flavours/glitch/features/status/components/detailed_status.jsx
@@ -134,10 +134,11 @@ class DetailedStatus extends ImmutablePureComponent {
 
     const fields = status.getIn(['account', 'fields']);
     let pronouns = null;
+    const pronounFields = ['pronouns', 'pronoun', 'professional noun', 'pronoun(s)'];
 
     if (fields) {
       fields.forEach(field => {
-        if (field.get('name') === 'pronouns') {
+        if (field.get('name') && pronounFields.includes(field.get('name').toLowerCase())) {
           pronouns = field.get('value');
         }
       });
